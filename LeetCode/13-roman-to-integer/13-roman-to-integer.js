d21113c230999fab66543ae6ea35115ea464b562
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-09-22 12:53:00 +0900

    Time: 264 ms (17.39%), Space: 46.3 MB (94.18%) - LeetHub

--- LeetCode/13-roman-to-integer/13-roman-to-integer.js
+++ LeetCode/13-roman-to-integer/13-roman-to-integer.js
@@ -3,32 +3,14 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-let result = 0;
-  let romanToNumsArray = Array.from(s).map((el) => {
-    switch (el) {
-      case 'I':
-        return 1;
-      case 'V':
-        return 5;
-      case 'X':
-        return 10;
-      case 'L':
-        return 50;
-      case 'C':
-        return 100;
-      case 'D':
-        return 500;
-      case 'M':
-        return 1000;
-    }
-  });
-  romanToNumsArray.forEach((el, idx) => {
-    if (romanToNumsArray[idx] < romanToNumsArray[idx + 1]) {
-      result -= romanToNumsArray[idx];
-    } else {
-      result += romanToNumsArray[idx];
-    }
-  });
-  return result;
-  return result;
+ const dictionary = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  return [...s].reduce((prev, cur, idx, Array) => (prev += (dictionary[cur] < dictionary[Array[idx + 1]] ?? 0 ? -1 : 1) * dictionary[cur]), 0)
 };
