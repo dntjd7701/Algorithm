be9e579a5527d86a1f72d0db2c26317faf6e93e6
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-09-27 09:38:52 +0900

    Time: 122 ms (5.64%), Space: 42.1 MB (72.85%) - LeetHub

---
+++ LeetCode/35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let idx;
+  idx = nums.findIndex((val, idx) => {
+    if (!nums.includes(target)) {
+      return val > target;
+    }
+    return val === target;
+  });
+
+  return idx === -1 ? nums.length : idx;
+};
