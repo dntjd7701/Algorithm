6ba3ff215929a202da23bfdbe4ef425887846632
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-09-29 16:27:20 +0900

    Time: 91 ms (80.72%), Space: 43.4 MB (78.46%) - LeetHub

--- LeetCode/136-single-number/136-single-number.js
+++ LeetCode/136-single-number/136-single-number.js
@@ -1,15 +1,27 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var singleNumber = function(nums) {
+//   let storage = [];
+//   while (nums.length > 0) {
+//     let curEl = nums.shift();
+//     if (!nums.includes(curEl) && !storage.includes(curEl)) {
+//       return curEl;
+//     } else {
+//       storage.push(curEl);
+//     }
+//   }
+// };
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var singleNumber = function(nums) {
-  let storage = [];
-  while (nums.length > 0) {
-    let curEl = nums.shift();
-    if (!nums.includes(curEl) && !storage.includes(curEl)) {
-      return curEl;
-    } else {
-      storage.push(curEl);
-    }
-  }
+  let num = 0
+ for(let i=0; i<nums.length; i++){
+     num = num ^ nums[i];
+ }
+    return num;
 };
