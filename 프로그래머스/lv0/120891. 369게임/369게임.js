d708b7e6cef117a08b2d104e0770b50014e5606d
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-10-27 17:23:13 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,4 +1,4 @@
 function solution(order) {
-     return [...order.toString()].filter((el) => el === '3' || el === '6' || el === '9').length;
-
+    return [...order.toString()].filter((el) => el === '3' || el === '6' || el === '9').length;
+  //return order.toString().match(/[3|6|9]/g).length;
 }
