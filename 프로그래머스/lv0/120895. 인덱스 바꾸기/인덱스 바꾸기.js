66c19a17b373f89f3665a74a489c45fefc9fcc53
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-10-27 11:17:00 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,8 +1,5 @@
 function solution(my_string, num1, num2) {
-    let result = [...my_string];
-  let c1 = result.splice(num1, 1, '');
-  let c2 = result.splice(num2, 1, '');
-  result.splice(num1, 1, ...c2);
-  result.splice(num2, 1, ...c1);
-  return result.join('');
+my_string = my_string.split('');
+  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+  return my_string.join('');
 }
