c35438c79a0962aebaec641079e3b43597dd7a1c
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-11-24 19:56:59 +0900

    [level 0] Title: 이진수 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,8 +1,44 @@
 function solution(bin1, bin2) {
- let sum = parseInt(bin1, 2) + parseInt(bin2, 2);
+ // let sum = parseInt(bin1, 2) + parseInt(bin2, 2);
+ //  if (sum === 0) return '0';
+ //  let arr = [];
+ //  while (sum !== 0) {
+ //    arr.push(sum % 2);
+ //    sum = Math.floor(sum / 2);
+ //  }
+ //  return arr.reverse().join('');
+    
+    let sum = 0;
+  [...bin1].reverse().forEach((el, idx) => {
+    if (el === '0') return;
+    if (idx === 0) {
+      sum += 1;
+      return;
+    }
+    let value = 1;
+    for (let i = 0; i < idx; i++) {
+      value *= 2;
+    }
+    sum += value;
+  });
+
+  [...bin2].reverse().forEach((el, idx) => {
+    if (el === '0') return;
+    if (idx === 0) {
+      sum += 1;
+      return;
+    }
+    let value = 1;
+    for (let i = 0; i < idx; i++) {
+      value *= 2;
+    }
+    sum += value;
+  });
+
   if (sum === 0) return '0';
+
   let arr = [];
-  while (sum !== 0) {
+  while (sum > 0) {
     arr.push(sum % 2);
     sum = Math.floor(sum / 2);
   }
