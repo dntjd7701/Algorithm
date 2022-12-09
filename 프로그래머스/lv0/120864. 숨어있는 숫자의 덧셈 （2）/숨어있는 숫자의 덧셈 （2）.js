2fd54ca9b0e369be857ea505601a93f19a265140
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-12-09 18:14:27 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,8 +1,3 @@
 function solution(my_string) {
-      const newString = my_string
-    .replace(/[a-z,A-Z]/g, '/')
-    .split('/')
-    .filter((el) => el !== '');
-  if (newString.length === 0) return 0;
-  return +newString.reduce((acc, cur) => +acc + +cur);
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + +cur, 0);
 }
