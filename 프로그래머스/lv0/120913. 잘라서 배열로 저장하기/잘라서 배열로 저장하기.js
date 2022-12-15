dba52b9fbe6788872ac703995fb8c80e77430d9f
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2022-12-15 11:22:02 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,9 @@
+function solution(my_str, n) {
+   let arr = [];
+  let [prev, cur] = [0, n];
+  for (let i = 0; i < Math.ceil(my_str.length / n); i++) {
+    arr.push(my_str.slice(prev, cur));
+    [prev, cur] = [cur, cur + n];
+  }
+  return my_str.length === n ? [my_str] : n === 1 ? [...my_str] : arr;
+}
