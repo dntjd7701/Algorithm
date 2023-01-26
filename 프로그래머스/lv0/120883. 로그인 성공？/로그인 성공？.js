656a5bb6300c04cd41a9b152549e82f178909f61
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2023-01-26 13:45:36 +0900

    [level 0] Title: 로그인 성공?, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,6 +1,4 @@
 function solution(id_pw, db) {
-     const id = db.find((el) => el[0] === id_pw[0]);
-  if (!id) return 'fail';
-  if (id[1] !== id_pw[1]) return 'wrong pw';
-  return 'login';
+ const correctId = db.find((el) => el[0] === id_pw[0]);
+  return !correctId ? 'fail' : correctId[1] !== id_pw[1] ? 'wrong pw' : 'login';
 }
