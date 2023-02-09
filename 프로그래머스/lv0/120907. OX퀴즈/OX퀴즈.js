0cddc4286d07b7d7d5d433cd5fc461e61805a10a
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2023-02-09 20:08:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,13 +1,6 @@
 function solution(quiz) {
-return quiz.map((el) => {
-    const arr = el.split(' ');
-    let rst = '';
-    console.log(+arr[0] - +arr[2] === +arr[4]);
-    if (arr[1] === '-') {
-      rst = +arr[0] - +arr[2] === +arr[4] ? 'O' : 'X';
-    } else {
-      rst = +arr[0] + +arr[2] === +arr[4] ? 'O' : 'X';
-    }
-    return rst;
+ return quiz.map((el) => {
+    const [expression, compare] = el.split('=');
+    return eval(expression) === +compare ? 'O' : 'X';
   });
 }
