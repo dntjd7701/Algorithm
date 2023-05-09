5d06280d12ffe1ea8f96bdb1eda001448bdb5411
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2023-05-09 19:20:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1.27 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,19 @@
 function solution(numbers, target) {
-    var answer = 0;
-    return answer;
+  let answer = 0;
+  // DFS 사용, LIFO 방식 사용. 
+  function dfs(idxCnt, sum) {
+    //최종 결과로 모든 수를 다 썼을 때 결과가 나와야 함으로. 
+    if(numbers.length === idxCnt) {
+        if(target === sum) {
+            ++answer;
+        }
+        return;
+    }
+    dfs(idxCnt + 1, sum + numbers[idxCnt]);
+    dfs(idxCnt + 1, sum - numbers[idxCnt]);
+  }
+
+  //최상위 루트, 0, 0부터 시작 
+  dfs(0, 0);
+  return answer;
 }
