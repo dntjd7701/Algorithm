8f59e332ad94dbd9b0cabd7cd0814d55d176e13e
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2023-10-12 16:35:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 120 ms, Memory: 10612 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,21 @@
+function josephusPermutation(N, K) {
+  // 초기에 N명의 사람들을 나타내는 배열 생성
+  const people = Array.from({ length: N }, (_, index) => index + 1);
+  // 결과를 저장할 배열 초기화
+  const result = [];
+  // 현재 인덱스
+  let currentIndex = 0;
+  // N명의 사람이 모두 제거될 때까지 반복
+  while (people.length > 0) {
+    // K번째 사람을 제거하고 결과 배열에 추가
+    currentIndex = (currentIndex + K - 1) % people.length;
+    result.push(people.splice(currentIndex, 1)[0]);
+  }
+
+  return result;
+}
+
+// 예시: (7, 3)-요세푸스 순열
+const [N,K] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map((v) => +v);
+const result = josephusPermutation(N, K);
+console.debug(`<${result.join(', ')}>`);
