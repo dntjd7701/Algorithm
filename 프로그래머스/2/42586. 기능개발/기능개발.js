4f1c4096fa39f425bfd263936a51674788491bd3
WOOSUNG <71601453+dntjd7701@users.noreply.github.com>
2025-06-12 22:31:55 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,24 +1,24 @@
 function solution(progresses, speeds) {
-    const days = progresses.map((progress, idx) =>
-        Math.ceil((100 - progress) / speeds[idx])
-    );
+    const answer = [];
+    const days = progresses.map((progress, index) => {
+        return Math.ceil((100 - progress) / speeds[index]);
+    });
 
-    const result = [];
     let currentDay = days[0];
     let count = 1;
 
     for (let i = 1; i < days.length; i++) {
         if (days[i] <= currentDay) {
-            // 이전 배포일에 같이 배포 가능
             count++;
         } else {
-            // 새로운 배포 시작
-            result.push(count);
+            answer.push(count);
             currentDay = days[i];
             count = 1;
         }
     }
 
-    result.push(count); // 마지막 배포 묶음 추가
-    return result;
+    // 마지막 묶음 추가
+    answer.push(count);
+
+    return answer;
 }
